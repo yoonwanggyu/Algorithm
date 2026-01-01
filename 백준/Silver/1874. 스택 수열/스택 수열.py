c9829f8c20b71154a6b30450a01a55fcf1eb0a88
Wang's Sweet Home <yog3467@naver.com>
2026-01-01 15:20:43 +0900

    [Silver II] Title: 스택 수열, Time: 188 ms, Memory: 38876 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,37 +1,41 @@
 import sys
 input = sys.stdin.readline
 
-# 스택 문제 - 후입선출
 N = int(input())
-A = [0] * N # 수열 리스트 
+A = []
 for i in range(N):
-    A[i] = int(input()) # ex) [4,3,6,8,7,5,2,1]
+    A.append(int(input()))
     
-stack = [] # 쌓을 빈스택
-num = 1 # 현재 수열 값과 비교할 자연수
-result = True
-answer = ""
+stack = []    # 비교하면 쌓을 스택
+num = 1
+answer = []
+result = True    # No 부분에서 break를 걸어도 마지막에 print가 찍히기 때문에 result가 True인 경우만 마지막 print 찍히게
 
-# 문제 풀이 코드
+# 현재 값이랑 수열 값이랑 비교하면서 넣엏다 뺏다 진행
+# 현재 값이 수열보다 클 경우 작을 경우 나눠서 생각
 for i in range(N):
     now_su = A[i]
-    if now_su >= num: # 값이 같아질 때까지 stack에 append    
-        while A[i] >= num:
+    
+    if now_su >= num:         # 값이 같아질 때까지 stack에 append  
+        while now_su >= num:
             stack.append(num)
             num += 1
-            answer += "+\n" 
-        stack.pop() # now_su랑 num이 같으면 빼기
-        answer += "-\n"
-    else: # 현재 수열값이 자연수보다 작으면 빼기
-        n = stack.pop()
-        if n > now_su:
+            answer.append('+')
+        stack.pop()           # 값이 같아지면 pop 빼기
+        answer.append('-')
+        
+    else:                     # 현재 수열값이 자연수보다 작으면 빼기
+        if not stack:          # 스택 비었는지 체크
             print("NO")
             result = False
             break
-        else:
-            answer += "-\n"
+        n = stack.pop()
+        if n != now_su:        # 반드시 같아야 함
+            print("NO")
+            result = False
+            break
+        answer.append('-')
+            
 if result:
-    print(answer)
-        
-    
-    
+    for i in answer:
+        print(i)
